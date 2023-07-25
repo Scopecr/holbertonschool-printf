49a57f7b23e87c0ccacd4390573885fc36158455
nadeshda18 <nadeshda02@hotmail.com>
2023-07-25 13:49:00 +0000

    Updated

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef PRINTF
 #define PRINTF
 int _printf(const char *format, ...);
+int _strlen(const char *str);
+char* _itoa(int num);
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
