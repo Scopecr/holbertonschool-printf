8132f000a8c0940872ae451469fc7725f3b866fc
nadeshda18 <nadeshda02@hotmail.com>
2023-07-25 17:14:15 +0000

    Update

--- _printf.c
+++ _printf.c
@@ -1,136 +1,96 @@
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 /**
- * _strlen - length of a string
- * @str: pointer
+ * _strlen - Calculate the length of a string.
+ * @str: Pointer to the string.
  *
- * Return: length of the string
+ * Return: Length of the string.
  */
 int _strlen(const char *str)
 {
-	int len = 0;
+    int len = 0;
 
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+    return (len);
 }
 
 /**
- * _itoa - convert an integer to a string
- * @num: number
+ * _write_char - Write a character to stdout.
+ * @c: The character to write.
  *
- * Return: the string representation of the number
- */
-char *_itoa(int num)
-{
-	int count = 0;
-	int temp = num;
-	int i;
-
-	char *str = (char *)malloc((count + 1) * sizeof(char));
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-
-	while (temp != 0)
-	{
-		temp /= 10;
-		count++;
-	}
-
-	for (i = count - 1; i >= 0; i--)
-	{
-		str[i] = '0' + (num % 10);
-		num /= 10;
-	}
-	str[count] = '\0';
-	return (str);
-}
-
-/**
- * _write_char - write a character to stdout
- * @c: the character to write
- *
- * Return: number of characters written (1 in this case)
+ * Return: Number of characters written (1 in this case).
  */
 int _write_char(char c)
 {
-	return (write(STDOUT_FILENO, &c, 1));
+    return (write(STDOUT_FILENO, &c, 1));
 }
 
 /**
- * _write_str - write a string to stdout
- * @str: pointer to the string to write
+ * _write_str - Write a string to stdout.
+ * @str: Pointer to the string to write.
  *
- * Return: number of characters written (excluding the null byte)
+ * Return: Number of characters written (excluding the null byte).
  */
 int _write_str(char *str)
 {
-	int len = _strlen(str);
-	return (write(STDOUT_FILENO, str, len));
+    int len = _strlen(str);
+    return (write(STDOUT_FILENO, str, len));
 }
 
 /**
- * _printf - print output to stdout according to a format
- * @format: pointer to the format string
+ * _printf - Print output to stdout according to a format.
+ * @format: Pointer to the format string.
  *
- * Return: the number of characters printed (excluding the null byte)
+ * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
-	int printed_chars = 0;
-	int i;
+    int printed_chars = 0;
+    int i;
 
-	va_list args;
-	va_start(args, format);
+    va_list args;
+    va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			printed_chars += _write_char(format[i]);
-		}
-		else
-		{
-			i++;
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            printed_chars += _write_char(format[i]);
+        }
+        else
+        {
+            i++;
 
-			switch (format[i])
-			{
-				case 'c':
-					{
-						char c = va_arg(args, int);
-						printed_chars += _write_char(c);
-						break;
-					}
-				case 's':
-					{
-						char *str = va_arg(args, char*);
-						printed_chars += _write_str(str);
-						break;
-					}
-				case '%':
-					{
-						printed_chars += _write_char('%');
-						break;
-					}
-				default:
-					{
-						printed_chars += _write_char('%');
-						printed_chars += _write_char(format[i]);
-						break;
-					}
-			}
-		}
-	}
+            if (format[i] == '\0')
+                break;
 
-	va_end(args);
+            if (format[i] == 'c')
+            {
+                char c = va_arg(args, int);
+                printed_chars += _write_char(c);
+            }
+            else if (format[i] == 's')
+            {
+                char *str = va_arg(args, char*);
+                printed_chars += _write_str(str);
+            }
+            else if (format[i] == '%')
+            {
+                printed_chars += _write_char('%');
+            }
+            else
+            {
+                printed_chars += _write_char('%');
+                printed_chars += _write_char(format[i]);
+            }
+        }
+    }
 
-	return (printed_chars);
+    va_end(args);
+
+    return (printed_chars);
 }
-
