66ec05485ce01c3927c6dcf12bf9a09d411651e9
nadeshda18 <nadeshda02@hotmail.com>
2023-07-26 02:40:08 +0000

    handler

---
+++ handlers2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+
+const ConvSpecifierInfo convHandlers[] =
+{
+    {'c', &print_char},
+    {'s', &print_string},
+    {'%', &print_percent},
+    {'r', &print_custom_r}
+};
