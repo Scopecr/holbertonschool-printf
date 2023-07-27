099e070ba2a2b9d83896d8c1f4fc114483c13f3a
Scopecr <wjrcarrion@gmail.com>
2023-07-27 00:40:51 +0000

    updatedfiles

--- main.h
+++ main.h
@@ -5,11 +5,13 @@
 
 typedef struct ConvSpecifierInfo ConvSpecifierInfo;
 
+int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
 int print_anything(va_list args);
+int print_bi(unsigned int n);
 
 struct ConvSpecifierInfo
 {
---
+++ print_bi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *
+ */
+
+int print_bi(unsigned int n)
+{
+	unsigned a, b, c, d, f;
+	int count = 0;
+
+	b = n;
+	c = 1;
+	for (a = 1; b > 1; a++)
+	{
+		b = b / 2;
+		c = c * 2;
+	}
+	for (f = 1; f <= a; f++)
+	{
+		d = n / c;
+		n = n % c;
+		c = c / 2;
+		_putchar(d + '0');
+		count++;
+	}
+	return (count);
+}
