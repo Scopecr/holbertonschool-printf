53553a01a59d8a7e65aa9ab2726d3d0dfa5b5dc0
Scopecr <wjrcarrion@gmail.com>
2023-07-27 02:38:52 +0000

    hex

---
+++ print_hexadecimal.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_hexalow - convert unsigned int in hexadecimal
+ * @n: unsigned int
+ * Return: numer times print
+ */
+
+int print_hexalow(unsigned int n)
+{
+	int i, j, reminder. count = 0;
+	unsigned int num_copy = n;
+	char *hexadecimal;
+
+	if (n == 0)
+		return (_putchar('0'));
+	while (num_copy ! = 0)
+	{
+		num_copy /= 16;
+		count++;
+	}
+	hexadecimal = malloc(count);
+	for (i = 0; n != 0; i++)
+	{
+		remainder = n % 16;
+		if (reminder < 10)
+		{
+			hexadecimal[i] = reminder + '0';
+		}
+		else
+		{
+			hexadecimal[i] = reminder + 'W';
+		}
+		n /= 16;
+	}
+	for (j = i - 1;j >= 0; j --)
+		_putchar(hexadecimal[j]);
+	free(hexadecimal);
+	return (count);
+}
