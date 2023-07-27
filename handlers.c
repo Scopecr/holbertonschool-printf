2bd0fac503559b0edae43947fe9db221ed9d79e5
nadeshda18 <nadeshda02@hotmail.com>
2023-07-27 21:46:01 +0000

    update

--- handlers.c
+++ handlers.c
@@ -9,7 +9,9 @@ const ConvSpecifierInfo convHandlers[] =
 	{'c', &print_char},
 	{'s', &print_string},
 	{'%', &print_percent},
-	{'r', &print_anything}
+	{'r', &print_anything},
+	{'i', &print_i},
+	{'d', &print_d}
 };
 
 /**
@@ -20,9 +22,12 @@ const ConvSpecifierInfo convHandlers[] =
  */
 int print_char(va_list args)
 {
+	int count = 0;
 	int c = va_arg(args, int);
 
+	count++;
 	return (write(1, &c, 1));
+	return(count);
 }
 
 /**
@@ -40,6 +45,7 @@ int print_string(va_list args)
 	{
 		count += write(1, str++, 1);
 	}
+	count++;
 	return (count);
 }
 /**
@@ -50,8 +56,11 @@ int print_string(va_list args)
  */
 int print_percent(va_list args)
 {
+	int count = 0;
 	(void)args;
-	return (write(1, "%", 1));
+	count++;
+	return(write(1, "%", 1));
+	return(count);
 }
 /**
  * print_anything - print any character
@@ -64,12 +73,93 @@ int print_anything(va_list args)
 	int count = 0;
 	const char *format = va_arg(args, const char *);
 
+	const char c = *format;
 	count += write(1, "%", 1);
 
-	if (*format == '%')
+	format++;
+	if (c != '%')
 	{
-		count += write(1, format, 1);
+		count += write(1, &c, 1);
 	}
 
-	return (count);
+	count++;
+	return count;
+}
+
+/**
+ * print_int - print an integer value
+ * @args: arguments
+ *
+ * Return: len
+ */
+int print_i(va_list args)
+{
+	int num_str[12];
+	int num;
+	int len = 0;
+	int i;
+
+	num = va_arg(args, int);
+
+	if (num == 0)
+	{
+		return _putchar('0');
+	}
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	while (num != 0)
+	{
+		num_str[len++] = num % 10 + '0';
+		num /= 10;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(num_str[i]);
+	}
+	return (len);
+}
+
+/**
+ * print_d - print decimal numbers
+ * @args: arguments
+ *
+ * Return: len
+ */
+int print_d(va_list args)
+{
+	int num_str[12];
+    int num;
+    int len = 0;
+    int i;
+
+	num = va_arg(args, int);
+
+    if (num == 0)
+    {
+        return _putchar('0');
+    }
+
+    if (num < 0)
+    {
+        _putchar('-');
+        num = -num;
+    }
+
+    while (num != 0)
+    {
+        num_str[len++] = num % 10 + '0';
+        num /= 10;
+    }
+
+    for (i = len - 1; i >= 0; i--)
+    {
+        _putchar(num_str[i]);
+    }
+    return len;
 }
